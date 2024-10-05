935327618245ff9b6ed0c425a9ffbc917b445a2d
Ananta Manoranjan <78996823+Ananta-dot@users.noreply.github.com>
2024-10-05 19:12:21 +0400

    second

---
+++ src/CompanyName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CompanyName.css';
+
+function CompanyName() {
+  return (
+    <div className="company-name">
+      (un)certain
+    </div>
+  );
+}
+
+export default CompanyName;
